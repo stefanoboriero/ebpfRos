b7c407e03661ae7fea417bcb8d346dd3e6fbc1b6
StefanoBoriero <stefano.boriero@gmail.com>
2024-06-08 15:59:32 +0200

    feat: publish counter of messages sent on each topic

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/ringbuf"
 	"github.com/cilium/ebpf/rlimit"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
 )
 
 type node_creation_counterDataT struct {
@@ -24,12 +27,89 @@ type node_creation_counterDataT struct {
 	Path    [16]uint8
 }
 
+type topic_message_counterDataT struct {
+	Pid       uint32
+	Uid       uint32
+	TopicName [32]uint8
+}
+
 func main() {
 	setupOtelSdk(context.Background())
 	// Remove resource limits for kernels <5.11.
 	if err := rlimit.RemoveMemlock(); err != nil {
 		log.Fatal("Removing memlock:", err)
 	}
+    var wg sync.WaitGroup
+    wg.Add(1)
+	go setupNodeCreationCounter(&wg)
+    wg.Add(1)
+    go setupTopicMessageCounter(&wg)
+	log.Println("Setup complete")
+    wg.Wait()
+}
+
+func setupTopicMessageCounter(wg *sync.WaitGroup) {
+	var objs topic_message_counterObjects
+	if err := loadTopic_message_counterObjects(&objs, nil); err != nil {
+		log.Fatal("Loading eBPF objects:", err)
+	}
+	defer objs.Close()
+
+	ex, _ := link.OpenExecutable("/opt/ros/humble/lib/librmw_implementation.so")
+	link, err := ex.Uprobe("rmw_publish", objs.TopicMessageCount, nil)
+	if err != nil {
+		log.Fatal("Attaching XDP:", err)
+	}
+	defer link.Close()
+	stopper := make(chan os.Signal, 5)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
+
+	ringbuf_reader, err := ringbuf.NewReader(objs.TopicMessageOutput)
+	if err != nil {
+		log.Fatalf("Error opening ringbuf reader %s", err)
+	}
+	defer ringbuf_reader.Close()
+
+	go func() {
+		<-stopper
+
+		if err := ringbuf_reader.Close(); err != nil {
+			log.Fatalf("Error closing ringbuffer, %s", err)
+		}
+        wg.Done()
+	}()
+
+	var meter = otel.Meter("my-service-meter")
+	topicMessageCounter, err := meter.Int64Counter("topic.message.sent")
+	if err != nil {
+		log.Fatalf("Unable to create Otel counter, %s", err)
+	}
+	log.Println("Waiting for topic messages...")
+
+	var data topic_message_counterDataT
+	for {
+		record, err := ringbuf_reader.Read()
+		if err != nil {
+			if errors.Is(err, ringbuf.ErrClosed) {
+				log.Println("Received signal, exiting..")
+				return
+			}
+			log.Printf("Error reading from reader: %s", err)
+			continue
+		}
+		// Parse the ringbuf event entry into a bpfEvent structure.
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &data); err != nil {
+			log.Printf("parsing ringbuf event: %s", err)
+			continue
+		}
+
+		log.Printf("pid: %d\ttopicName: %s\n", data.Pid, data.TopicName)
+        topicNameAttribute := attribute.String("topic.name", string(data.TopicName[:]))
+		topicMessageCounter.Add(context.TODO(), 1, metric.WithAttributes(topicNameAttribute))
+	}
+}
+
+func setupNodeCreationCounter(wg *sync.WaitGroup) {
 	var objs node_creation_counterObjects
 	if err := loadNode_creation_counterObjects(&objs, nil); err != nil {
 		log.Fatal("Loading eBPF objects:", err)
@@ -54,6 +134,7 @@ func main() {
 	go func() {
 		<-stopper
 
+        wg.Done()
 		if err := ringbuf_reader.Close(); err != nil {
 			log.Fatalf("Error closing ringbuffer, %s", err)
 		}
@@ -64,7 +145,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to create Otel counter, %s", err)
 	}
-	log.Println("Setup OTel and waiting for events...")
+	log.Println("Waiting for nodes to be create...")
 
 	var data node_creation_counterDataT
 	for {
@@ -83,8 +164,7 @@ func main() {
 			continue
 		}
 
-		log.Printf("pid: %d\tcomm: %s\n", data.Pid, data.Path)
-		log.Printf("Incrementing counter")
+		log.Printf("pid: %d\tnodeName: %s\n", data.Pid, data.Path)
 		nodeCounter.Add(context.TODO(), 1)
 	}
 }
